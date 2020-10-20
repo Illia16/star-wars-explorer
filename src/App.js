b36577552a957c8e9af11e927569909b6b8c121e
Illia <illia2143@gmail.com>
2020-10-19 23:37:18 -0400

    app router #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect, Suspense, lazy }from 'react';
 import { BrowserRouter as Switch, Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from './Components/Header';
-import People from './Components/People';
+// import People from './Components/People';
 
+const People = lazy( () => import('./Components/People'));
 
 function App() {
+  // changing input what user wants to search
+  const [searchQuery, setInput] = useState(null);
+  // saving the corresponding results from API call
+  const [results, getData] = useState({people: null, films: null, planets: null});
+  const [isLoading, setLoading] = useState(false);
+
+  const userChoice = (e) => {
+    setInput(e.target.name);
+  }
+
+  useEffect( () => {
+    if (searchQuery) {
+      setLoading(true)
+      axios({
+          url: `https://swapi.dev/api/${searchQuery}`,
+          method: 'GET',
+      })
+      .then( (res) => { 
+        getData({...results, [searchQuery]: res.data}) 
+        setLoading(false)
+      })
+      .catch( error => { console.log(error); })
+    }
+  }, [searchQuery]);
+
   return (
     <>
-      <Switch>
-        <Route exact path="/">
-          <Header />
-          <NavLink to="/people">people</NavLink>
-        </Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <NavLink to="/people" name="people" onClick={ e => userChoice(e) } >people</NavLink>
+            <NavLink to="/people" name="films" onClick={  e => userChoice(e) } >movies</NavLink>
+            <NavLink to="/people" name="planets" onClick={  e => userChoice(e) } >planets</NavLink>
+          </Route>
 
-        <Route path="/people">
-          <People />
-        </Route>
-      </Switch>
+          <Route path='/people'>
+            <People
+              res={results.people}
+              loadingStatus = {isLoading}
+            />
+
+            {/* <Movies props={results[searchQuery]}/>
+            <Planets props={results[searchQuery]}/> */}
+          </Route>
+        </Switch>
+      </Suspense>
     </>
   );
 }
--- src/Components/People.js
+++ src/Components/People.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {BrowserRouter as Switch, Route, NavLink } from 'react-router-dom';
 
-const People = () => {
+const People = (props) => {
+    console.log(props);
 
     const PeopleRes = () => {
         return(
-            <div>People from Star Wars</div>
+            <>
+                <h2>People from Star Wars</h2>
+                <ul className="listOfPeople">
+                    {
+                        !props.loadingStatus ?
+                        props.res.results.map((person) => {
+                            return (
+                                <>
+                                    {/* <li key={person.name}>{person.name}</li> */}
+                                    <NavLink to={`/${person.name}`} key={person.name}>{person.name}</NavLink>
+                                </>
+                            )
+                        })
+                        : <div>Loading...</div>
+                    }
+                </ul>
+                <NavLink to="/">Back to main menu</NavLink>
+            </>
         )
     };
     
