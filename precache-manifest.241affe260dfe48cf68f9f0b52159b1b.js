a3c6c831c1cd2fe3ae8dcf2acad508f406bff315
Illia <illia2143@gmail.com>
2020-10-20 22:10:36 -0400

    Updates

---
+++ precache-manifest.241affe260dfe48cf68f9f0b52159b1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4375ef496740ae5418f64ec23c4943b",
+    "url": "/starWarsExplorer/index.html"
+  },
+  {
+    "revision": "f1afb9e8f1597bfe158c",
+    "url": "/starWarsExplorer/static/css/main.212d0c09.chunk.css"
+  },
+  {
+    "revision": "81852c6cf108e94ab1cf",
+    "url": "/starWarsExplorer/static/js/2.cbbc222a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/starWarsExplorer/static/js/2.cbbc222a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1afb9e8f1597bfe158c",
+    "url": "/starWarsExplorer/static/js/main.0c615abc.chunk.js"
+  },
+  {
+    "revision": "3a4570cbe9147d571830",
+    "url": "/starWarsExplorer/static/js/runtime-main.d2aea5b7.js"
+  }
+]);
