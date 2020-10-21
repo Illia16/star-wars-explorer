51abec9be1eac4accd3b926d45b7a05e05a8ecc1
Illia <illia2143@gmail.com>
2020-10-21 00:29:55 -0400

    gh-pages #2

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -5,11 +5,11 @@ const MainMenu = ( props ) => {
     const { userChoice } = props;
 
     return(
-        <Route exact path="/">
+        <>
             <li><NavLink to="/people" name="people" onClick={ userChoice } >People</NavLink></li>
             <li><NavLink to="/films" name="films" onClick={  userChoice } >Movies</NavLink></li>
             <li><NavLink to="/planets" name="planets" onClick={userChoice } >Planets</NavLink></li>
-        </Route>
+        </>
     );
 };
 
